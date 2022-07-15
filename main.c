824ece8eac14d04f03564bfb7773a3b90ebaaca3
TanQinYang <108197858+TanQinYang@users.noreply.github.com>
2022-07-15 22:33:15 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+/*{
+    int a=5;
+    int b=4;
+    printf("%d",a|b);
+    return 0;
+}
+ */
+ {
+ int a =45;
+ int b=10;
+ int c=0;
+ c=(a+b,a+c,b+c);
+ printf("%d",c);
+     return 0;
+ }
